8ba4922a08d5c913a75530d6c2abe92f0c599e2f
Sean Leather <sean.leather@gmail.com>
2019-04-21 08:28:03 +0200

    Add CFLAGS to compile command

--- build.rs
+++ build.rs
@@ -73,6 +73,9 @@ fn use_installed() {
 
     // Link to some installed `graphite2` shared library and build a simple executable.
     cmd.arg("-lgraphite2").arg("-o").arg("/dev/null").arg("graphite2/tests/examples/simple.c");
+    if let Some(cflags) = option_env!("CFLAGS") {
+        cmd.env("CFLAGS", cflags);
+    }
     println!("[build.rs] Command: {:?}", cmd);
 
     // Run the command and check the status.
